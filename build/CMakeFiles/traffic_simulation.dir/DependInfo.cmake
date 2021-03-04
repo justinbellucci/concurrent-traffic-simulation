16ccec37a0218dfb0c37ac0657c11d7951040c08
justinbellucci <justin.bellucci@me.com>
2021-03-04 11:27:00 -0700

    initial upload from Udacity

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/Graphics.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/Intersection.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/Street.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/TrafficLight.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/TrafficObject.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/TrafficSimulator-Final.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/src/Vehicle.cpp" "/Users/justinbellucci/GitHub/concurrent-traffic-simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CERES_FOUND"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.5.1_3/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
